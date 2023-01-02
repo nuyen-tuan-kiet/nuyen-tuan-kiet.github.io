42414a12c9cf57451a00c2f7059d7cd594b5e737
tuan-kiet <89498672+nuyen-tuan-kiet@users.noreply.github.com>
2023-01-02 14:35:26 +0700

    Update vertex.js

--- vertex.js
+++ vertex.js
@@ -5,14 +5,6 @@ var size_small = 7; // kich thuoc do thi
 // thiết lập vị trí các node
 var X_POS_LOGICAL = [800, 725, 875, 650, 950, 725, 875, 800];
 var Y_POS_LOGICAL = [25, 125, 125, 225, 225, 325, 325, 425];
-var SMALL_CURVE = [[0, 0.001, 0, 0.5, -0.5, 0, 0, 0],
-								  [0, 0, 0, 0.001, 0, 0.001, -0.2, 0],
-								  [0, 0.001, 0, 0, 0, 0.2, 0, 0],
-								  [-0.5, 0, 0, 0, 0, 0.001, 0, 0.5],
-								  [0.5, 0, 0, 0, 0, 0, 0, -0.5],
-								  [0, 0, -0.2, 0, 0, 0, 0.001, 0.001],
-								  [0, 0.2, 0, 0, 0, 0, 0, 0],
-								  [0, 0, 0, -0.5, 0.5, 0, 0, 0]]
 
 
 
@@ -24,13 +16,10 @@ function vertexCircle (id, lable){
       this.minHeightDiff = 3;
       this.range = 5;
       this.font =ctx.font =  '15px sans-serif';
-	  this.path= ctx.beginPath();
-	  this.arc= ctx.arc(X_POS_LOGICAL[id], Y_POS_LOGICAL[id], 20, 0, 2 * Math.PI);
-	  this.stroke= ctx.stroke();
-	  this.text= ctx.fillText(id, X_POS_LOGICAL[id],Y_POS_LOGICAL[id] );
-
-
-
+      this.path= ctx.beginPath();
+      this.arc= ctx.arc(X_POS_LOGICAL[id], Y_POS_LOGICAL[id], 20, 0, 2 * Math.PI);
+      this.stroke= ctx.stroke();
+      this.text= ctx.fillText(id, X_POS_LOGICAL[id],Y_POS_LOGICAL[id] );
 }
 
 
