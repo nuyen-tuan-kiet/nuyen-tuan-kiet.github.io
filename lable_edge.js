f70f9748f244d262ef5d6b6e81877d0d0d4e1a2e
tuan-kiet <89498672+nuyen-tuan-kiet@users.noreply.github.com>
2023-01-02 14:38:09 +0700

    Update lable_edge.js

--- lable_edge.js
+++ lable_edge.js
@@ -1,6 +1,6 @@
 
 
- function Lable(value, dest_x,dest_y){
+ function Lable(value, dest_x, dest_y){
     this.font = ctx.font = "15px Arial";
     this.text = ctx.fillText(value, dest_x, dest_y);    
     this.value_edge=value
