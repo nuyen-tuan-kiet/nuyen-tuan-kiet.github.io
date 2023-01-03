12a1d462f9841f079a1fb568d23b57fe218f5269
tuan-kiet <89498672+nuyen-tuan-kiet@users.noreply.github.com>
2023-01-03 17:33:58 +0700

    Update vertex.js

--- vertex.js
+++ vertex.js
@@ -2,7 +2,7 @@
 
 var size_small = 7; // kich thuoc do thi
 
-// thiết lập vị trí các node
+// thiết lập vị trí các đỉnh
 var X_POS_LOGICAL = [800, 725, 875, 650, 950, 725, 875, 800];
 var Y_POS_LOGICAL = [25, 125, 125, 225, 225, 325, 325, 425];
 
